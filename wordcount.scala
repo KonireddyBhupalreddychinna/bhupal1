f091ed9af0e0dc5945075fd8592100a38408f37c
bhupal k <kbrchinna95@gmail.com>
2022-05-15 19:44:07 +0530

    added print schema commend in wordcount program

--- wordcount.scala
+++ wordcount.scala
@@ -1,2 +1,32 @@
-fjkadsf
-asdf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+
+object SampleProgram {
+  
+  def main(args: Array[String]){
+    val spark = SparkSession.builder()
+  .appName("simple name")
+  .master("local[*]")
+  .getOrCreate()
+  
+  
+val inputdf = spark.read
+.format("csv")
+.option("path", "D:/sparkdatasets/orders.csv")
+.option("header", true) 
+.option("inferSchema", true) 
+.load()
+
+
+inputdf.show()
+
+inputdf.printSchema()
+
+inputdf.show()
+    
+  }
+  
+
+  
+}
